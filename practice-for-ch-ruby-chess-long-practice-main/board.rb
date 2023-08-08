54c82b207d0290b350b3a3364005b3ab0cf75d38
“Julia <juliakahn1@gmail.com>
2023-08-08 13:56:43 -0700

    Fixed methods to add pieces: pawns

--- practice-for-ch-ruby-chess-long-practice-main/board.rb
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -1,4 +1,6 @@
-class Board 
+require_relative "piece.rb"
+
+class Board
 
     def initialize
         @rows = Array.new(8) {Array.new(8, nil)}
@@ -8,42 +10,53 @@ class Board
         add_bishops
         add_queens
         add_kings
+        puts rows
+    end
+
+    def [](pos)
+        row, col = pos
+        rows[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        rows[row][col] = val
     end
 
     def add_pawns
-        rows[2].each {|space| space = Piece.new} # future will be from Pawn class
+        rows[1].each {|space| space = "hello"} # future will be from Pawn class
         rows[6].each {|space| space = Piece.new} # future will be from Pawn class
     end
 
     def add_rooks
-        rows[[0, 7]] = Piece.new # will be rook
-        rows[[0,0]] = Piece.new # will be rook
-        rows[[7,7]] = Piece.new # will be rook
-        rows[[7,0]] = Piece.new # will be rook
+        self[[0,7]] = Piece.new # will be rook
+        self[[0,0]] = Piece.new # will be rook
+        self[[7,7]] = Piece.new # will be rook
+        self[[7,0]] = Piece.new # will be rook
     end
 
     def add_knights
-        rows[[0, 1]] = Piece.new 
-        rows[[0,6]] = Piece.new 
-        rows[[7,1]] = Piece.new 
-        rows[[7,6]] = Piece.new 
+        self[[0, 1]] = Piece.new
+        self[[0,6]] = Piece.new
+        self[[7,1]] = Piece.new
+        self[[7,6]] = Piece.new
     end
 
     def add_bishops
-        rows[[0, 2]] = Piece.new 
-        rows[[0,5]] = Piece.new 
-        rows[[7,2]] = Piece.new 
-        rows[[7,5]] = Piece.new 
+        self[[0, 2]] = Piece.new
+        self[[0,5]] = Piece.new
+        self[[7,2]] = Piece.new
+        self[[7,5]] = Piece.new
     end
 
     def add_queens
-        rows[[0,3]] = Piece.new 
-        rows[[7,3]] = Piece.new 
+        self[[0,3]] = Piece.new
+        self[[7,3]] = Piece.new
     end
 
     def add_kings
-        rows[[0,4]] = Piece.new 
-        rows[[7,4]] = Piece.new 
+        self[[0,4]] = Piece.new
+        self[[7,4]] = Piece.new
     end
 
     # def fill_empty_spaces
@@ -52,14 +65,6 @@ class Board
         # end
     # end
 
-    def [](pos)
-        row, col = pos 
-        @rows[row][col]
-    end
-    def []=(pos, val)
-        row, col = pos 
-        @rows[row][col] = val
-    end
     def move_piece(start_pos, end_pos)
         raise 'There is no piece in that spot' if rows[start_pos].nil?
         raise "This piece can't go there" if !rows[end_pos].nil? # not considering if piece was moved correctly
@@ -67,6 +72,6 @@ class Board
         rows[start_pos] = nil
     end
 
-    protected
+    # protected
     attr_reader :rows
 end
